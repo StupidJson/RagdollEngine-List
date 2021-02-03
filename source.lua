4784189e18e14850d9fe6ba5729245d5188b9eda
StupidJson <78207462+StupidJson@users.noreply.github.com>
2021-02-03 21:05:48 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -172,7 +172,7 @@ TsunioxGUI.MouseButton1Click:Connect(function()
 end)
 
 ExodusGUI.MouseButton1Click:Connect(function()
-	loadstring(game:HttpGet("https://pastebin.com/MeAL9tUE"), true)()
+	loadstring(game:HttpGet("https://pastebin.com/MeAL9tUE"))()
 end)
 
 -- Load Text:
